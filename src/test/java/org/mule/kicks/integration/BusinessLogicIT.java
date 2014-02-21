fb80e51c241e353f70913b82f212b5053548a095
Miguel Oliva <moliva@mulesoft.com>
2014-02-21 15:22:36 -0300

    Changed poll properties names

--- src/test/java/org/mule/kicks/integration/BusinessLogicIT.java
+++ src/test/java/org/mule/kicks/integration/BusinessLogicIT.java
@@ -56,10 +56,10 @@ public class BusinessLogicIT extends AbstractKickTestCase {
 		System.setProperty("page.size", "1000");
 
 		// Set the frequency between polls to 10 seconds
-		System.setProperty("polling.frequencyMillis", "10000");
+		System.setProperty("poll.frequencyMillis", "10000");
 
 		// Set the poll starting delay to 20 seconds
-		System.setProperty("polling.startDelayMillis", "20000");
+		System.setProperty("poll.startDelayMillis", "20000");
 
 		// Setting Default Watermark Expression to query SFDC with LastModifiedDate greater than ten seconds before current time
 		System.setProperty("watermark.default.expression", "#[groovy: new Date(System.currentTimeMillis() - 10000).format(\"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'\", TimeZone.getTimeZone('UTC'))]");
